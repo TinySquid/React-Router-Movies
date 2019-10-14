b54cbb382e4a4df92d60cc2077bf822b281e0498
Michael Nunes <miketnunes@gmail.com>
2019-10-14 13:08:48 -0700

    Add ability to click movie and get details

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,24 +2,24 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+  const [movie, setMovie] = useState(null);
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        console.log(response)
+        setMovie(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
-  },[]);
-  
+  }, [props.match.params.id]);
+
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -27,6 +27,7 @@ const Movie = (props) => {
   // }
 
   if (!movie) {
+    console.log('Data not loaded yet.')
     return <div>Loading movie information...</div>;
   }
 
@@ -42,7 +43,6 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
         {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
